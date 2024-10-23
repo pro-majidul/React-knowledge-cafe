0fa05999ce638e33c4bd2ad7b29100acfa32d7e7
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-10-23 20:51:01 +0600

    react knowledge cafe template create

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h3 className='bg-orange-300 text-3xl text-center'>Reack Knowledge Cafe</h3>
+    </>
+  )
+}
+
+export default App
